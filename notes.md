a3d4838ffb909bdee0aaffe4e418686eb18c423d
samir gyawali <gyawali.samir000@gmail.com>
2024-09-06 15:30:02 +0545

    getting things ready for card component

--- notes.md
+++ notes.md
@@ -4,3 +4,4 @@
     50:27
     1:04:11 react router for switching components
     1:22:24 designing filter sections and cards
+    1:28:16 handling each item
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './card.scss'
 
-const Card = () => {
+const Card = (item) => {
   return (
-    <div className='card'>Card</div>
+    <div className='card'>
+      <Link to={`/${item.id}`} className="imageContainer">
+        <img src={item.img} alt="" />
+      </Link>
+      <div className="textContainer"></div>
+    </div>
   )
 }
 
